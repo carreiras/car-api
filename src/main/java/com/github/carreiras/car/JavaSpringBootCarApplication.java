7306d2edf0a1142decdd0b2c8324f0edd8e51b8b
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-03-21 11:48:45 -0300

    first commit

---
+++ src/main/java/com/github/carreiras/car/JavaSpringBootCarApplication.java
@@ -0,0 +1,13 @@
+package com.github.carreiras.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/carreiras/car/JavaSpringBootCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.carreiras.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
